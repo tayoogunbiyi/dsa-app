96526cc66a1f134117934fc91454f2397247d8bd
Eyitayo <eyitayoogunbiyi@gmail.com>
2019-12-30 14:25:59 +0100

    added christmas emojis

--- js/main.js
+++ js/main.js
@@ -1,32 +1,33 @@
-const Prando = require('prando').default;
-const { members } = require('./constants')
-const { getNearestMonday,getNextSunday,deterministicShuffle} = require('./utils');
+const Prando = require("prando").default;
+const { members } = require("./constants");
+const {
+  getNearestMonday,
+  getNextSunday,
+  deterministicShuffle
+} = require("./utils");
 
-
-function getWeekRangeString(){
-	return getNearestMonday() + " - " + getNextSunday()
+function getWeekRangeString() {
+  return getNearestMonday() + " - " + getNextSunday();
 }
 
-function generateRandomPairings(){
-	let rng = new Prando(getWeekRangeString());
-	const pairings = []
-	const shuffledMembers = deterministicShuffle(members,rng)
-	for(let i =0 ; i < shuffledMembers.length ; i+=2){
-		const pairing = `<li>${shuffledMembers[i]} - ${shuffledMembers[i+1]}</li>`;
-		pairings.push(pairing)
-	}
+function generateRandomPairings() {
+  let rng = new Prando(getWeekRangeString());
+  const pairings = [];
+  const shuffledMembers = deterministicShuffle(members, rng);
+  for (let i = 0; i < shuffledMembers.length; i += 2) {
+    const pairing = `<li>${shuffledMembers[i]} - ${
+      shuffledMembers[i + 1]
+    }</li>`;
+    pairings.push(pairing);
+  }
 
-	const element = document.getElementById("app")
-	let content = '<div>'
-	content += `<h2> Pairings for the week ${getWeekRangeString()}</h2>`
-	content += '<ul>'
-	pairings.forEach((pair) => content+= pair)
-	content += '</ul>'
-	element.innerHTML = content
-
+  const element = document.getElementById("app");
+  let content = "<div>";
+  content += `<h2> 🎅 Pairings for the week ${getWeekRangeString()}</h2>`;
+  content += "<ul>";
+  pairings.forEach(pair => (content += pair));
+  content += "</ul>";
+  element.innerHTML = content;
 }
 
-generateRandomPairings()
-
-
-
+generateRandomPairings();
