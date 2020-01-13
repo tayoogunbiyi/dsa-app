d33b2b618c24fccd86cbfcb780fdcd03ea661619
Eyitayo <eyitayoogunbiyi@gmail.com>
2020-01-13 06:53:09 +0100

    fixed bug involving when the usage starts

--- js/constants.js
+++ js/constants.js
@@ -3,6 +3,6 @@ module.exports = {
     SET_1: ["Tayo", "Samad", "Dare", "Yahaya", "Ifere", "Joseph Olabisi"],
     SET_2: ["Nachi", "Dotun", "David", "Irene", "Olumide", "Moore"]
   },
-  REFERENCE_DATE_STRING: "January 6, 2020",
+  REFERENCE_DATE_STRING: "January 13, 2020",
   SEED: "oqiefi1293919qnwkasdasd0s"
 };
