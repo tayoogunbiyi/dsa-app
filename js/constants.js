c4b4f608ad1b45cf0d09fe16e8e2cb4216a31419
Eyitayo Ogunbiyi <eyitayoogunbiyi@gmail.com>
2020-02-02 20:58:37 +0100

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,7 +1,7 @@
 module.exports = {
   members: {
-    SET_1: ["Tayo", "Samad", "Dare", "Yahaya", "Ifere", "Joseph Olabisi"],
-    SET_2: ["Nachi", "Dotun", "David", "Irene", "Olumide", "Moore"]
+    SET_1: ["Tayo", "Samad", "Dare", "Yahaya", "Ifere", "Joseph Olabisi","Caleb"],
+    SET_2: ["Nachi", "Dotun", "David", "Irene", "Olumide", "Moore","Tobi"]
   },
   REFERENCE_DATE_STRING: "January 13, 2020",
   SEED: "oqiefi1293919qnwkasdasd0s"
