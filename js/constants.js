1b3afaede145327f58099e88fc638f92bea378bc
Eyitayo <eyitayoogunbiyi@gmail.com>
2020-02-04 17:35:20 +0100

    updated member to timi

--- js/constants.js
+++ js/constants.js
@@ -1,7 +1,15 @@
 module.exports = {
   members: {
-    SET_1: ["Tayo", "Samad", "Dare", "Yahaya", "Ifere", "Joseph Olabisi","Caleb"],
-    SET_2: ["Nachi", "Dotun", "David", "Irene", "Olumide", "Moore","Tobi"]
+    SET_1: [
+      "Tayo",
+      "Samad",
+      "Dare",
+      "Yahaya",
+      "Ifere",
+      "Joseph Olabisi",
+      "Caleb"
+    ],
+    SET_2: ["Nachi", "Dotun", "David", "Irene", "Timi Tayo", "Moore", "Tobi"]
   },
   REFERENCE_DATE_STRING: "January 13, 2020",
   SEED: "oqiefi1293919qnwkasdasd0s"
