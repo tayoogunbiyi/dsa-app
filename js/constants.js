953a57d40ede16914ec775c1aa7d2c8c0f381a4c
Eyitayo Ogunbiyi <eyitayoogunbiyi@gmail.com>
2020-04-12 23:24:32 +0100

    pushed new changes with new people

--- js/constants.js
+++ js/constants.js
@@ -1,14 +1,16 @@
 module.exports = {
   members: {
-    SET_1: ["Tayo", "Samad", "Dare", "Yahaya", "Ifere", "Samuel", "Caleb"],
+    SET_1: ["Tayo",  "David Popoola","Doyin", "Paul Ofili", "Jade", "Samuel", "Caleb","Abubakar","Joe Olabisi"],
     SET_2: [
-      "Patrick Ofili",
-      "Paul Ofili",
-      "David",
-      "Doyin",
-      "Timi Tayo",
+      "Samad",
       "Moore",
-      "Tobi"
+      "Dare",
+      "Patrick Ofili",
+      "Timi Tayo",
+      "Yahaya",
+      "Tobi",
+      "Ifere",
+      "David Evbo"
     ]
   },
   REFERENCE_DATE_STRING: "January 13, 2020",
